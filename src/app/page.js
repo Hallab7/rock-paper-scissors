ca4378a0af882633b3df0f7a770864b5f9bc68dc
Hallab <ibrahimhabeebolawale@gmail.com>
2025-04-14 01:10:46 +0100

    modified

--- src/app/page.js
+++ src/app/page.js
@@ -54,24 +54,35 @@ export default function Home() {
       <Header score={score} />
 
       <div className="grid place-items-center mt-20">
-        <div className="grid grid-cols-2 gap-10 relative">
-          {choices.map((choice, index) => (
-            <div
-              key={index}
-              onClick={() => handleChoice(choice.name)}
-              className={`bg-[#fafafa] w-32 h-32 rounded-full border-[12px] flex justify-center items-center cursor-pointer transition-transform transform hover:scale-110 ${choice.border}`}
-            >
-              <Image
-                src={choice.image}
-                alt={choice.name}
-                className="w-[50px] h-[50px]"
-              />
-            </div>
-          ))}
+  <div className="relative w-[350px] h-[318.6px]">
+    {choices.map((choice, index) => {
+      let positionClass = '';
 
-          
+      if (index === 0) {
+        positionClass = 'absolute top-0 left-0';
+      } else if (index === 1) {
+        positionClass = 'absolute top-0 right-0';
+      } else if (index === 2) {
+        positionClass = 'absolute bottom-0 left-1/2 transform -translate-x-1/2';
+      }
+
+      return (
+        <div
+          key={index}
+          onClick={() => handleChoice(choice.name)}
+          className={`bg-[#fafafa] w-32 h-32 rounded-full border-[12px] flex justify-center items-center cursor-pointer transition-transform hover:scale-110 ${positionClass} ${choice.border}`}
+        >
+          <Image
+            src={choice.image}
+            alt={choice.name}
+            className="w-[50px] h-[50px]"
+          />
         </div>
-      </div>
+      );
+    })}
+  </div>
+</div>
+
 
       <button className="absolute bottom-4 md:right-4 right-1/2 border px-4 py-2 rounded-md text-sm">
         RULES
