73341ce3812ac58e1537ea42e8202e82dd460ef7
Hallab <ibrahimhabeebolawale@gmail.com>
2025-08-20 19:49:55 +0100

    fix

--- src/app/game-room/page.js
+++ src/app/game-room/page.js
@@ -11,7 +11,7 @@ import { motion } from "framer-motion";
 import { MdArrowBack } from "react-icons/md";
 import Link from "next/link";
 
-const SOCKET_URL = "http://localhost:4000"
+const SOCKET_URL = "process.env.NEXT_PUBLIC_SOCKET_URL"
 
 const normalizeMove = (m) => {
   if (!m) return null;
